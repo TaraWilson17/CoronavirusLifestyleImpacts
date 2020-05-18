92431e699b351058ef7181b1bfaf456d3debba1f
Tara Wilson <wwtara@hotmail.com>
2020-05-17 20:52:51 -0700

    Renames data generator test script

--- CoronavirusLifestyleImpacts/src/test_data_generator.py
+++ CoronavirusLifestyleImpacts/src/test_data_generator.py
@@ -1,22 +1,22 @@
 import unittest
-import data_generator
+from data_generator import DataGenerator
 
 class UnitTests(unittest.TestCase):
-    
+
     def test_covid_data_is_not_empty(self):
         self.assertFalse(covid_data.empty)
-        
+
     def test_trend_data_is_not_empty(self):
         self.assertFalse(trend_data.empty)
-        
+
     def test_trend_data_has_expected_columns(self):
         expected_columns = keywords + ["isPartial"]
         self.assertCountEqual(list(trend_data.columns), expected_columns)
-        
+
     def test_covid_data_is_for_correct_state(self):
         state_col = list(covid_data.AdminRegion1)
         self.assertListEqual(state_col, [state] * len(state_col))
-        
+
     def test_covid_data_contains_needed_columns(self):
         expected_columns = ["Updated", "Deaths", "Recovered", "AdminRegion1"]
         self.assertTrue(all(x in list(covid_data.columns) for x in expected_columns))
@@ -25,5 +25,5 @@ class UnitTests(unittest.TestCase):
 if __name__ == '__main__':
     state = "Washington"
     keywords = ["Bars near me", "Home workouts"]
-    covid_data, trend_data = data_generator.get_data(state, keywords)
+    covid_data, trend_data = DataGenerator.get_data(state, keywords)
     unittest.main()
