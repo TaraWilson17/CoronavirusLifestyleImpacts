cdcb95e3f222dbd76f8109296dbe7dd55db0909c
Tara Wilson <wwtara@hotmail.com>
2020-05-24 11:01:55 -0700

    Removes saving pdf copy

--- coronavirus_lifestyle_impacts/data_visualizer.py
+++ coronavirus_lifestyle_impacts/data_visualizer.py
@@ -10,12 +10,12 @@ class DataVisualizer:
         self.draw_graph(self.data_frame)
 
     def draw_graph(self, data_frame):
+        
         N = 50
         x = np.random.rand(N)
         y = np.random.rand(N)
         colors = np.random.rand(N)
-        area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+        area = (30 * np.random.rand(N))**2 
 
         plt.scatter(x, y, s=area, c=colors, alpha=0.5)
         plt.savefig("test_image.png")
-        plt.savefig("test_image.pdf")
