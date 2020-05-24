3dde29b4c8ec45697b6d47da9da93ce14b63d1cb
Tara Wilson <wwtara@hotmail.com>
2020-05-24 10:53:05 -0700

    Adds proof of concept visualization creation

--- coronavirus_lifestyle_impacts/data_visualizer.py
+++ coronavirus_lifestyle_impacts/data_visualizer.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class DataVisualizer:
     def __init__(self, data_frame):
         self.data_frame = data_frame
@@ -7,4 +10,12 @@ class DataVisualizer:
         self.draw_graph(self.data_frame)
 
     def draw_graph(self, data_frame):
-        pass
+        N = 50
+        x = np.random.rand(N)
+        y = np.random.rand(N)
+        colors = np.random.rand(N)
+        area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+        plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+        plt.savefig("test_image.png")
+        plt.savefig("test_image.pdf")
