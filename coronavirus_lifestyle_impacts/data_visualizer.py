448b2253d3aa4c6e7cb419a43909c3485f5ea0e1
Tara Wilson <wwtara@hotmail.com>
2020-05-26 20:48:30 -0700

    Makes data visualization from aggregated covid and trends data

--- coronavirus_lifestyle_impacts/data_visualizer.py
+++ coronavirus_lifestyle_impacts/data_visualizer.py
@@ -5,19 +5,28 @@ class DataVisualizer:
     def __init__(self, state, keywords, all_data):
         self.state = state
         self.keywords = keywords
-        self.all_data = all_data
+        self.df = all_data
 
     def show(self):
         print("Rendering visualization...")
         self.draw_graph()
 
     def draw_graph(self):
+        #'Date', 'Confirmed', 'ConfirmedChange', 'Deaths', 'DeathsChange', 'Recovered', 'RecoveredChange', 'Country', 'State', 'Bars near me', 'Home workouts']
         
-        N = 50
-        x = np.random.rand(N)
-        y = np.random.rand(N)
-        colors = np.random.rand(N)
-        area = (30 * np.random.rand(N))**2 
-
-        plt.scatter(x, y, s=area, c=colors, alpha=0.5)
-        plt.savefig("test_image.png")
+        fig, ax1 = plt.subplots(figsize=(12,8))
+        plt.title("Keyword Search Comparison in " + self.state)
+        ax1.set_ylabel("Relative Popularity", color="blue")
+        
+        for keyword in self.keywords:
+            ax1.plot(self.df.Date, self.df[keyword], "--", c="blue", label="Searches for " + keyword)
+        
+        ax1.tick_params(axis='y', labelcolor="blue")
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("Coronavirus cases", color="red")
+        ax2.plot(self.df.Date, self.df.Confirmed, c="red")
+        ax2.tick_params(axis='y', labelcolor="red")
+        ax1.legend()
+        
+        file_name = self.state + "_coronavirus_trend_impacts.png"
+        plt.savefig(file_name)
