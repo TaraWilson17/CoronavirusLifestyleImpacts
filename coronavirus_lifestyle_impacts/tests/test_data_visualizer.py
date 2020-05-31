7ee0fc9a5ad32cf4a5482ddf9f0ef3e5fc1e6050
Tara Wilson <wwtara@hotmail.com>
2020-05-31 12:14:13 -0700

    Fixes image path name

--- coronavirus_lifestyle_impacts/tests/test_data_visualizer.py
+++ coronavirus_lifestyle_impacts/tests/test_data_visualizer.py
@@ -26,7 +26,7 @@ class UnitTests(unittest.TestCase):
         self.data_processor.run()
         
     def test_visualization_generated(self):
-        filepath = "../" + self.state + "_coronavirus_trend_impacts_lines.png"
+        filepath = "../" + self.state + "_coronavirus_trend_impacts.png"
         self.assertTrue(os.path.isfile(filepath))
 
 if __name__ == '__main__':
