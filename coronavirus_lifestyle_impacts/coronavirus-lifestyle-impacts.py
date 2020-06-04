d2bffeac27fe83655d786b925f2898cf365046d7
Tara Wilson <wwtara@hotmail.com>
2020-06-03 21:03:53 -0700

    Adds print for arguments

--- coronavirus_lifestyle_impacts/coronavirus-lifestyle-impacts.py
+++ coronavirus_lifestyle_impacts/coronavirus-lifestyle-impacts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from cmd_parser import CmdParser
 from data_generator import DataGenerator
 from data_processor import DataProcessor
@@ -10,24 +11,30 @@ from data_visualizer import DataVisualizer
 
 cmd_parser = CmdParser()
 cmd_parser.parse(sys.argv[1:])
-print("Using input: {}, {}".format(cmd_parser.state, cmd_parser.keywords))
+print("Using input: state={}, keywords={}".format(cmd_parser.state, cmd_parser.keywords))
 
 data_generator = DataGenerator(cmd_parser.state, cmd_parser.keywords)
 data_generator.run()
-print("\n=========Sampling data========")
+print("\n=========Generating data========")
 print(data_generator.covid_data.head(5))
 print(data_generator.trend_data.head(5))
 
 data_frames = [data_generator.covid_data, data_generator.trend_data]
 
+print("\n=========Processing data========")
 data_processor = DataProcessor(data_generator.keywords, data_frames)
 data_processor.run()
 print("\nProcessed COVID Data columns\n", data_processor.clean_data_frame[0].columns)
 print("\nProcessed GoogleTrends Data columns\n", data_processor.clean_data_frame[1].columns)
 agg_data_frame = data_processor.agg_data_frame
-agg_data_frame.to_csv('agg_data_frame.csv')
-print("\nAggregated cleaned dataframe saved as agg_data_frame.csv\n")
+curr_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+output_dir = curr_dir + "/outputs/"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+file_path = output_dir + data_generator.state + "_agg_data_frame.csv"
+agg_data_frame.to_csv(file_path)
+print("\nAggregated cleaned dataframe saved at {}\n".format(file_path))
 
-
+print("\n=========Visualizing data========")
 data_visualizer = DataVisualizer(data_generator.state, data_generator.keywords, data_processor.agg_data_frame)
 data_visualizer.show()
